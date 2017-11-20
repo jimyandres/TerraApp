3e07567c131f7fcae92f7f6cfbfe7cbf3b5d44e2
jimyandres <jimyandres@utp.edu.co>
2017-11-20 17:08:28 -0500

    Carousel added for diplay (from static source) images

--- App/Containers/ProductScreen.js
+++ App/Containers/ProductScreen.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react'
-import { BackHandler } from 'react-native'
+import { BackHandler, View, Image, Dimensions, StatusBar } from 'react-native'
 import { Content, Container, Header, Left, Right, Body, Button, Text, Title, Icon } from 'native-base'
 import { connect } from 'react-redux'
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
+
+import I18n from '../I18n';
+import Carousel, { ParallaxImage, Pagination } from 'react-native-snap-carousel';
 
 // Styles
-import styles from './Styles/ProductScreenStyle'
+import styles from './Styles/ProductScreenStyle';
+import Colors from '../Themes/Colors';
+
+const { width, height } = Dimensions.get('window');
 
 class ProductScreen extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: { width, height },
+      slider1ActiveSlide: 1,
+      slider1Ref: null
+    };
+  }
+
+  _onLayoutDidChange = (e) => {
+    const layout = e.nativeEvent.layout;
+    this.setState({ size: { width: layout.width, height: layout.height } });
+  }
+
   componentDidMount () {
     BackHandler.addEventListener('hardwareBackPress', () => {
       this.props.navigation.goBack();
       return true
     })
   }
+
+  renderImagesCarousel = ({item, index}, parallaxProps) => {
+    // console.error(item.image);
+      return (
+        <View style={styles.slide}>
+          <Image style={styles.image} source={{ uri: item.image }} />
+        </View>
+      );
+    }
+
   render () {
     const {goBack} = this.props.navigation;
+    const arrayImages = [
+      {
+        "image": "https://terra-quimbaya-storage.s3-us-west-2.amazonaws.com/products/1443887620/1443887620_2017_10_01_015529_0.jpeg"
+      },
+      {
+        "image": "https://terra-quimbaya-storage.s3-us-west-2.amazonaws.com/products/1443887620/1443887620_2017_10_01_015529_1.jpeg"
+      },
+      {
+        "image": "https://terra-quimbaya-storage.s3-us-west-2.amazonaws.com/products/1443887620/1443887620_2017_10_01_015529_2.jpeg"
+      }
+    ];
+    const { slider1ActiveSlide, slider1Ref } = this.state;
     return (
       <Container>
-        <Header>
+        <Header style={styles.header}>
+          <StatusBar backgroundColor={Colors.primaryStatusBar} />
           <Left>
             <Button
               transparent
@@ -35,8 +78,55 @@ class ProductScreen extends Component {
 
         </Header>
 
-        <Content padder>
-          <Text>ProductScreen Content</Text>
+        <Content style={{ paddingTop: 5 }}>
+          <View style={{height: 250, alignItems: 'center'}}>
+            <Carousel
+              ref={(c) => { if (!this.state.slider1Ref) { this.setState({ slider1Ref: c }); } }}
+              data={arrayImages}
+              renderItem={this.renderImagesCarousel}
+              sliderWidth={this.state.size.width }
+              itemWidth={this.state.size.width - 10}
+              loop={true}
+              autoplay={true}
+              autoplayInterval={3000}
+              autoplayDelay={5000}
+              onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index }) }
+            />
+          </View>
+
+          <Pagination
+            dotsLength={arrayImages.length}
+            activeDotIndex={slider1ActiveSlide}
+            containerStyle={styles.paginationContainer}
+            dotColor="#0F6D38"
+            dotStyle={styles.paginationDot}
+            inactiveDotColor="#000"
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+            carouselRef={slider1Ref}
+            tappableDots={!!slider1Ref}
+          />
+
+          <View style={{ paddingHorizontal: 10}}>
+            <Text style={{ paddingVertical:15}}>
+              {I18n.t('by')}
+              <Text style={[styles.linkProducer]}>
+                 {', '}*Productor*
+              </Text>
+            </Text>
+
+            <Text style={styles.productName}>
+               *Nombre producto*
+            </Text>
+
+            <View>
+              <Text>*Calificacion*</Text>
+            </View>
+
+            <Text>*Precio*</Text>
+
+          </View>
+
         </Content>
       </Container>
     )
--- App/Containers/Styles/ProductScreenStyle.js
+++ App/Containers/Styles/ProductScreenStyle.js
@@ -1,6 +1,43 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
-
+import { StyleSheet, Dimensions } from 'react-native'
+import { ApplicationStyles, Colors } from '../../Themes/'
+const entryBorderRadius = 8;
+const win = Dimensions.get('window');
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  slide: {
+    height: 250,
+  },
+  header: {
+    backgroundColor: Colors.primary,
+  },
+  image: {
+    flex: 1,
+    width: win.width -10,
+    height: win.height,
+    borderRadius: 8,
+  },
+  imageContainer: {
+    height: 200,
+    backgroundColor: 'white',
+    borderTopLeftRadius: entryBorderRadius,
+    borderTopRightRadius: entryBorderRadius
+  },
+  paginationContainer: {
+    paddingVertical: 8
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 8
+  },
+  linkProducer: {
+    fontSize: 15,
+    color: '#0F6D38',
+    fontWeight: 'bold',
+  },
+  productName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 })
